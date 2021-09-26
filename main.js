aba632d5ba596bcea9fbc5562a7bc6fc3598fd8d
Bankole David <Tomibanks57@gmail.com>
2021-09-26 19:06:33 +0100

    Form Validation

--- main.js
+++ main.js
@@ -1,58 +1,51 @@
- function tocheck() {
-  var fn = document.getElementById("firstname").value;
-  var ln = document.getElementById("lastname").value;
-  var em = document.getElementById("email").value;
-  var p1 = document.getElementById("password1").value;
-  var p2 = document.getElementById("password2").value;
+function tocheck() {
+    const firstName = document.getElementById("firstname").value;
+    const emailInput = document.getElementById("email").value;
+    const passWord = document.getElementById("passWord1").value;
+    const passWord2 = document.getElementById("password2").value;
 
-//error message for length
-  if (p1 === "" || p1.length < 6) {
-   document.getElementById("p1").innerHTML = "Password must be at least 6 characters";
-   document.getElementById("password1").style.borderColor = "red";
-  }
-  else {
-   document.getElementById("password1").style.borderColor = "green";
-   document.getElementById("p1").innerHTML = "";
-  }
+ //error message for length
+    if (passWord === "" || passWord.length < 6) {
+      document.getElementById("passWord").innerHTML = "Password must be at least 6 characters";
 
-  //error message for password match
-  if (p1 !== p2 || p2 === "") {
-   document.getElementById("p2").innerHTML = "Passwords do not match";
-   document.getElementById("password1").style.borderColor = "red";
-   document.getElementById("password2").style.borderColor = "red";
-  }
-  else {
-   document.getElementById("password2").style.borderColor = "green";
-   document.getElementById("p2").innerHTML = "";
-  }
+      document.getElementById("passWord1").style.borderColor = "red";
+    }
+    else {
 
-  //error message for first name
-  if (fn === "") {
-   document.getElementById("fn").innerHTML = "Please enter a valid name";
-   document.getElementById("firstname").style.borderColor = "red";
-  }
-  else {
-   document.getElementById("firstname").style.borderColor = "green";
-   document.getElementById("fn").innerHTML = "";
-  }
+      document.getElementById("passWord1").style.borderColor = "green";
+      document.getElementById("passWord").innerHTML = "";
+    }
 
-  //error message for last name
-  if (ln === "") {
-   document.getElementById("ln").innerHTML = "Please enter a valid name";
-   document.getElementById("lastname").style.borderColor = "red";
-  }
-  else {
-   document.getElementById("lastname").style.borderColor = "green";
-   document.getElementById("ln").innerHTML = "";
-  }
+    //error message for passoword match
+    if (passWord !== passWord2 || passWord2 === "" || passWord2.length < 6) {
+      document.getElementById("passWord2").innerHTML = "Passwords do not match";
 
-  //error message for email
-  if (em === "") {
-   document.getElementById("em").innerHTML = "Please enter a valid email";
-   document.getElementById("em").style.borderColor = "red";
+      document.getElementById("passWord1").style.borderColor = "red";
+      document.getElementById("password2").style.borderColor = "red";
+    }
+    else {
+
+      document.getElementById("password2").style.borderColor = "green";
+      document.getElementById("passWord2").innerHTML = "";
+    }
+
+    //error message for firstName
+    if (firstName === "") {
+      document.getElementById("firstName").innerHTML = "Please enter a valid name";
+      document.getElementById("firstname").style.borderColor = "red";
+    }
+    else {
+      document.getElementById("firstname").style.borderColor = "green";
+      document.getElementById("firstName").innerHTML = "";
+    }
+
+    //error message for email
+    if (emailInput === "") {
+      document.getElementById("emailInput").innerHTML = "Please enter a valid email";
+      document.getElementById("email").style.borderColor = "red";
+    }
+    else {
+      document.getElementById("email").style.borderColor = "green";
+      document.getElementById("emailInput").innerHTML = "";
+    }
   }
-  else {
-   document.getElementById("email").style.borderColor = "green";
-   document.getElementById("em").innerHTML = "";
-  }
- }
